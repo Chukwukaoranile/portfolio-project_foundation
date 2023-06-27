79def0670d8fea5f5894e574d40d6c19fe4b52f0
chukwukaoranile <prosperbinary@gmail.com>
2023-06-27 10:52:32 +0100

    authentication

--- website/views.py
+++ website/views.py
@@ -1,6 +1,78 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db, mail
+import json
+from flask_mail import Message
+from flask_mail import Mail
 
 views = Blueprint('views', __name__)
-@views.route('/')
+mail = Mail()
+
+77
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return "<p>Home</p>"
+    if request.method == 'POST': 
+        note = request.form.get('note')#Gets the note from the HTML 
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            db.session.add(new_note) #adding the note to the database 
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+            return jsonify({})
+
+
+
+@views.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+
+        # Send email
+        send_email(mail, name, email, message)
+
+        # Store in database
+        save_contact(name, email, message)
+
+        return "Thank you for contacting us! We will get back to you soon."
+
+    return render_template('contact.html', user=current_user)
+
+def send_email(mail, name, email, message):
+    msg = Message("Contact Form Submission", sender=email, recipients=["chika@chukwukaoranile.tech"])
+    msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    mail.send(msg)
+
+
+def save_contact(name, email, message):
+    # Save the contact form data in the database
+    # You can use the Note model or create a separate Contact model for this purpose
+    contact = Contact(name=name, email=email, message=message)
+    db.session.add(contact)
+    db.session.commit()
+
+
+@views.route("/about")
+def about():
+    return render_template("about.html", user=current_user)
