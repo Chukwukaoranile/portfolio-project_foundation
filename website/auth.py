348722fa7e3ebe159dd72926e0837ee962450052
chukwukaoranile <prosperbinary@gmail.com>
2023-07-07 17:05:39 +0100

    Flask structuring

--- website/auth.py
+++ website/auth.py
@@ -1,99 +1,98 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db, mail   ##means from __init__.py import db
-from flask_login import login_user, login_required, logout_user, current_user
-from flask_mail import Message
-
+from .models import Volunteer, Beneficiary
 
 auth = Blueprint('auth', __name__)
 
-
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
+@auth.route("/")
+def home():
+        return render_template("views.home")
+'''
+@auth.route('/beneficiary', methods=['GET', 'POST'])
+def beneficiary():
     if request.method == 'POST':
         email = request.form.get('email')
-        password = request.form.get('password')
+        name = request.form.get('Name')
+        phone_number = request.form.get('phone_number')
+        diagnoses = request.form.get('diagnoses')
+        message = request.form.get('message')
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
-        else:
-            flash('Email does not exist.', category='error')
-
-    return render_template("login.html", user=current_user)
-
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
-
-
-@auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
+        # To Check if any required field is missing
+        if not email or not name or not phone_number or not diagnoses or not message:
+            flash('Please fill in all the fields.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            # Rest of your code to create the Beneficiary instance
+            # ...
+            new_user = Volunteer(email=email, name=name, phone_number=phone_number, diagnoses=diagnoses)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Successful! Thanks for joining us. We will reach out to you soon', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("beneficiary.html")
+'''
+@auth.route('/beneficiary', methods=['GET', 'POST'])
+def beneficiary():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('Name')
+        phone_number = request.form.get('phone_number')
+        diagnoses = request.form.get('diagnoses')
+        message = request.form.get('message')
+
+        # To Check if any required field is missing
+        if not email or not name or not phone_number or not diagnoses or not message:
+            flash('Please fill in all the fields.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+        elif len(phone_number) < 4:
+            flash('Invalid Phone Number', category='error')
+        else:
+            new_user = Volunteer(email=email, name=name, phone_number=phone_number, diagnoses=diagnoses)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Successful! Thanks for joining us. We will reach out to you soon', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("beneficiary.html")
+
+
+@auth.route('/volunteer', methods=['GET', 'POST'])
+def volunteer():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        phone_number = request.form.get('phone_number')
+        address = request.form.get('address')
+        profession = request.form.get('profession')
+ 
+        user = Volunteer.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif not email or not name or not phone_number or not address or not profession:
+            flash('Please fill in all the fields.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(address) < 3:
+            flash('Invalid Address, must be greater than 3 characters.', category='error')
+        elif len(profession) < 2:
+            flash('Sorry no Abbrevation, proffession must be greater than 2 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+
+            new_user = Volunteer(email=email, name=name, phone_number=phone_number, address=address, profession=profession)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            flash('Successful! Thanks for joining us. We will reach out to you soon', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user=current_user)
-
-
-@auth.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        message = request.form.get('message')
-
-        # Send email
-        send_email(mail, name, email, message)
-
-        # Store in database
-        save_contact(name, email, message)
-
-        return "Thank you for contacting us! We will get back to you soon."
-
-    return render_template('views.contact')
-def send_email(mail, name, email, message):
-    msg = Message("Contact Form Submission", sender=email, recipients=["chika@chukwukaoranile.tech"])
-    msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
-    mail.send(msg)
-
-def save_contact(name, email, message):
-    # Save the contact form data in the database
-    # You can use the Note model or create a separate Contact model for this purpose
-    contact = Contact(name=name, email=email, message=message)
-    db.session.add(contact)
-    db.session.commit()
+    return render_template("volunteer.html")
 
 
 @auth.route("/about")
--- website/models.py
+++ website/models.py
@@ -3,20 +3,24 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-class User(db.Model, UserMixin):
+class Volunteer(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    name = db.Column(db.String(100))
+    phone_number = db.Column(db.Integer, unique=True)
+    address = db.Column(db.String(200))
+    profession = db.Column(db.String(50))
 
+
+class Beneficiary(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    name = db.Column(db.String(100))
+    phone_number = db.Column(db.Integer, unique=True)
+    diagnoses = db.Column(db.String(200))
+    message = db.Column(db.String(500))
+
+'''
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,14 +30,4 @@ class BlogPost(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User')
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(150))
-    message = db.Column(db.Text)
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-
-    def __init__(self, name, email, message):
-        self.name = name
-        self.email = email
-        self.message = message
+'''
